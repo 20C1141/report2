cd733d297851bf81e6cdf5bdac2777e2a917a40e
K20C1141 <67887230+20C1141@users.noreply.github.com>
2022-01-12 14:46:12 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -1,4 +1,5 @@
-mport rospy
+#!/usr/bin/env python3
+import rospy
 from std_msgs.msg import Int32
 
 rospy.init_node('count')
@@ -7,5 +8,5 @@ rate = rospy.Rate(10)
 n = 0
 while not rospy.is_shutdown():
         n += 1
-            pub.publish(n)
-                rate.sleep()
+        pub.publish(n)
+        rate.sleep()
